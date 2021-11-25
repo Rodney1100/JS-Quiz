6e04781e8e343314b5c5afaaa1acb25a4c83a8bf
Rodney P <rodney.plancher@gmail.com>
2021-11-24 19:43:17 -0500

    added notes and reload scores

--- js/jsScript.js
+++ js/jsScript.js
@@ -16,8 +16,10 @@ var formEl = document.getElementById("formHs");
 var vHSBtn = document.getElementById("vhighScores");
 var showScoreEl = document.getElementById("ShowScore");
 var highScoreList = []
-var shuffledQuestions, i;
-var time = 10
+var shuffledQuestions, i, enterNameMessage;
+// time the quiz will run
+var time = 5000
+    // questions for the quiz
 var questions = [{
         question: "What is my name?",
         answer: [{
@@ -119,98 +121,138 @@ var questions = [{
         ],
     },
 ];
+// this is out here because it needs to be global
 counting.classList.remove("timercount")
+    //  everything that needs to start the game after click
 var startGame = function() {
+    //  this gets the buttons off the screen
+    vHSBtn.classList.remove("hide");
+    deleteEl.classList.add("hide");
+    startBtnEl.classList.add("hide");
+    containerEl.classList.remove("hide");
     i = 0;
+    // passing the time to the time function
     countdown(time);
     counting.classList.add("timercount")
-    startBtnEl.classList.add("hide");
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    containerEl.classList.remove("hide");
     nextQuestion();
 };
-var hsScore = function(event) {
+// setting the score after the quiz
+var hsScore = function() {
     let hsItem = document.createElement("li")
     JSON.stringify(bodyEl.value)
     bodyEl.value.toUppercase
     hsItem.textContent = (bodyEl.value + " " + pointIncrament)
     highScoreList.push(hsItem.textContent)
-    highScoreList.push(bodyyEl.value)
     hsItem.innerHTML = "<p class='makeCenter'>Recent Score</p> </br> " +
         hsItem.textContent
     hsList.appendChild(hsItem)
     saveHighScoreList()
     bodyyEl.classList.add("hide");
-    console.log(highScoreList)
+    // console.log(highScoreList)
     replay()
 };
-
+// the y is for checking to see if the fucntion ran
+var y = 0
+    //  this is to change the color of the promp verification
+let redOrBlue = 0
+    // varifying that something was intered to the input
+var submitverification = function() {
+        if (bodyEl.value.length == 0) {
+            enterNameMessage = document.createElement("p")
+            enterNameMessage.setAttribute("id", "enterNameMessagePrompt")
+            enterNameMessage.textContent = ("Please write your initials!");
+            bodyyEl.appendChild(enterNameMessage)
+            bodyyEl.removeChild(enterNameEl);
+            bodyyEl.removeChild(bodyEl);
+            bodyyEl.removeChild(submitEl);
+            //  changing the color of the prompt
+            if ((redOrBlue % 2) == 0) {
+                enterNameMessage.classList.add("blue");
+                enterNameMessage.classList.remove("red");
+            } else {
+                enterNameMessage.classList.add("red");
+                enterNameMessage.classList.remove("blue");
+            }
+            // runs the same game
+            saveGame();
+            y++
+        } else {
+            hsScore();
+        }
+    }
+    // clears and reflesh the page
+function clearMessage() {}
+// replays the game
 function replay() {
     var replayEl = document.createElement("button");
     replayEl.className = "nameInput start-btn namein btnSub btn";
-    replayEl.textContent = "Play Again";
+    replayEl.textContent = "Redo Quiz";
     formEl.appendChild(replayEl);
-    // replayEl.addEventListener("click", saveHighScoreList)
+    replayEl.addEventListener("click", clearMessage)
 }
-
-var saveGame = function(event) {
+// sames the high score
+var saveGame = function() {
+    // hides buttons and input bars
     containerEl.classList.add("hide");
     wrongEl.classList.add("hide");
     rightEl.classList.add("hide");
+    // create the 
     enterNameEl = document.createElement("p");
     enterNameEl.innerHTML =
         "<p class='makeBig'> All done!</p> <p class='makeSmaller'>You Scored " +
         pointIncrament +
         "/" +
-        (shuffledQuestions.length) +
+        (shuffledQuestions.length - 1) +
         "</p>";
     enterNameEl.className = "nameInput";
     bodyyEl.appendChild(enterNameEl);
     bodyEl = document.createElement("input");
     bodyEl.className = "nameInput namein btnSub btn";
     bodyEl.placeholder = "place your initials here";
-    bodyEl.name = "initials"
+    bodyEl.name = "initials";
     bodyyEl.appendChild(bodyEl);
-    submitEl.classList.remove("hide")
+    submitEl.classList.remove("hide");
     submitEl.type = "submit";
-    bodyyEl.appendChild(submitEl)
-    counting.classList.remove("timercount")
-    counting.classList.add("hide")
-    while (bodyEl.value.length == 0) {
-        if (bodyEl.value.length == 0) {
-            console.log("Please write your initials")
-        } else {
-            submitEl.addEventListener("click", hsScore);
-        }
+    bodyyEl.appendChild(submitEl);
+    counting.classList.remove("timercount");
+    counting.classList.add("hide");
+    submitEl.addEventListener("click", submitverification);
+    if (y > 0) {
+        redOrBlue = Math.floor(Math.random() * 50);
+        bodyyEl.removeChild(enterNameMessage)
+
     }
-    // hsScore()
-};
+}
 
 var countdown = function(time) {
     var timeLeft = time;
     var timeInterval = setInterval(function() {
-        if ((i + 2) > shuffledQuestions.length) {
-            console.log("you are on q " + (i - 1))
-            console.log("there is suppose to be " + shuffledQuestions.length)
-            timeLeft -= timeLeft;
-        } else {}
-        pts = i;
-        if (timeLeft > 1) {
-            if (points == pts - 1) {} else {
-                timeLeft -= 3;
-                points = pts - 1;
+            if (i == questions.length) {
+                timeLeft -= timeLeft;
             }
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft--;
-        } else {
-            timerEl.textContent = "Finished";
-            clearInterval(timeInterval);
-            saveGame();
-        }
-    }, 1000);
+            if (vHSBtn.addEventListener("click", saveGame)) {
+                timeLeft -= timeLeft
+            }
+            pts = i;
+            if (timeLeft > 1) {
+                if (points !== pts - 1) {
+                    deleteEl.classList.add("hide")
+                    timeLeft -= 500;
+                    points = pts - 1;
+                }
+                timerEl.textContent = "Time: " + timeLeft;
+                timeLeft--;
+            } else if (timeLeft === 1) {
+                timerEl.textContent = "Time: " + timeLeft;
+                timeLeft--;
+            } else {
+                timerEl.textContent = "Finished";
+                clearInterval(timeInterval);
+                saveGame();
+            }
+        },
+        10);
 };
 
 var selectAnswers = function(e) {
@@ -226,9 +268,7 @@ var selectAnswers = function(e) {
 
 var nextQuestion = function() {
     resetState();
-    // console.log(i)
     showQuestion(shuffledQuestions[i]);
-    // console.log(shuffledQuestions[i]);
     i++;
 };
 var showQuestion = function(shuffledQuestions) {
@@ -249,14 +289,16 @@ var removeAnswer = function() {
     wrongEl.classList.add("hide");
     rightEl.classList.add("hide");
 };
+// reset the questionsanswers
 var resetState = function() {
     while (answerBtnEl.firstChild) {
         answerBtnEl.removeChild(answerBtnEl.firstChild);
     }
 };
+// points used for score and to check if there needing to subtract from time
 var points = 0;
 var pointIncrament = 0;
-
+// lets the user know if the answer is wrong
 function setStatusClass(correct) {
     if (correct) {
         rightEl.classList.remove("hide");
@@ -266,23 +308,51 @@ function setStatusClass(correct) {
         wrongEl.classList.remove("hide");
     }
 }
-
+// stores the scores in local storage
 function saveHighScoreList() {
     localStorage.setItem("highScoreList", JSON.stringify(highScoreList))
 }
+// delete the scores saved
+function deleting() {
+    highScoreList = []
+    saveHighScoreList()
+}
+// create the delete button
+var deleteEl = document.createElement("button");
 
+function deleteScore() {
+    vHSBtn.classList.add("hide")
+    deleteEl.className = ("btn hsBtn start-btn")
+    deleteEl.textContent = ("Delet High Scores")
+    hsList.appendChild(deleteEl)
+    deleteEl.addEventListener("click", deleting)
+}
+// loads the score back to the screen
 function loadHighScoreList() {
-    // var loadTask = function() {
-    var savedTasks = localStorage.getItem("tasks");
-    if (!savedTasks) {
+    deleteScore()
+        // brings the screen 
+    var savedScore = localStorage.getItem("highScoreList");
+    if (savedScore.length == 0) {
+        console.log("load game ran 1111");
         return false;
     }
-    savedTasks = JSON.parse(savedTasks)
-    console.log(savedTasks);
-    for (var i = 0; i < savedTasks.length; i++) { createTaskEl(savedTasks[i]) }
-    console.log(tasks[i]);
+    var topOfPage = document.getElementById("top-Of-Page");
+    console.log(topOfPage)
+    highScoreList = JSON.parse(savedScore)
+    var highScores = document.createElement("ul")
+    highScores.setAttribute = ("id", "HSList")
+    topOfPage.appendChild(highScores);
+    for (var i = 0; i < highScoreList.length; i++) {
+        var newli = document.createElement("li")
+        newli.className = ("btn str")
+        console.log(highScoreList[i]);
+        highScores.appendChild(newli)
+        newli.innerText = highScoreList[i];
+    }
+    console.log(!savedScore);
 }
-
+// the eventListeners that can run one of functions
+vHSBtn.addEventListener("click", loadHighScoreList)
 answerBtnEl.addEventListener("click", selectAnswers);
 startBtnEl.addEventListener("click", startGame);
 answerBtnEl.addEventListener("click", nextQuestion);
